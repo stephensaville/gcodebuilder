2e77837bebfc897b8cb250aff03bd48eaefc2693
zampire <zampire@gmail.com>
2021-01-24 23:52:57 -0800

    Switch to simplified "GCB" icon.

--- src/com/gcodebuilder/app/images/ImageResources.java
+++ src/com/gcodebuilder/app/images/ImageResources.java
@@ -37,13 +37,13 @@ public class ImageResources {
         return load(resourceName, false);
     }
 
-    public static final String ICON_RESOURCE_PREFIX = "gcode_builder_icon";
+    public static final String ICON_RESOURCE_PREFIX = "gcb_icon";
     public static final String ICON_RESOURCE_SUFFIX = ".png";
-    public static final int[] ICON_RESOURCE_SIZES = {64, 128, 256, 512, 1024};
+    public static final int[] ICON_RESOURCE_SIZES = {16, 32, 64, 128, 256, 512, 1024};
 
     public static List<Image> loadIcons(boolean backgroundLoading) {
         return Arrays.stream(ICON_RESOURCE_SIZES)
-                .mapToObj(size -> String.format("%s_%dx%d%s", ICON_RESOURCE_PREFIX, size, size, ICON_RESOURCE_SUFFIX))
+                .mapToObj(size -> String.format("%s_%d%s", ICON_RESOURCE_PREFIX, size, ICON_RESOURCE_SUFFIX))
                 .map(name -> load(name, backgroundLoading))
                 .collect(Collectors.toList());
     }
